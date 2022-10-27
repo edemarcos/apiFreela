d119dd69fab539791eef1c873d05e91a597b4fb3
edemarcos <edemarcos.rosetti@gmail.com>
2022-10-27 16:37:26 -0300

    ajustado configs

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,6 @@
-import express from 'express';
-import routes from './routes';
+import express  from 'express';
 
 const app = express();
 app.use(express.json());
-app.use(routes);
 
 export default app;
