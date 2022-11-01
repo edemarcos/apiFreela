0237e110c5dc85125be630744cbfd5089ec7250c
edemarcos <edemarcos.rosetti@gmail.com>
2022-10-31 21:03:05 -0300

    removido type orm e adicionado primsorm

--- src/app/usuario/dataproviders/models/usuario.model.ts
+++ src/app/usuario/dataproviders/models/usuario.model.ts
@@ -1,6 +1,6 @@
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
-@Entity('usuario')
+@Entity('usuarios')
 export class Usuario {
     @PrimaryGeneratedColumn("uuid")
     id: number
